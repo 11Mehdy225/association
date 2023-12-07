1ee60b1aa95f802d453eed4763e35370ea8e5449
unknown <loicmehdy11@outlook.fr>
2023-12-07 17:41:27 +0000

    scrollOK+BarremenuResponsive

--- js/js.js
+++ js/js.js
@@ -76,4 +76,41 @@ function slidePrev () { //fait defiler a gauche
     let decal = -slidesWidth * compteur ;
     elements.style.transform = `translateX(${decal}px)`;
     }
+
+  //notre menu avec la page reduite
+  const links = document.querySelectorAll("nav li");
+
+icons.addEventListener("click", () => {
+  nav.classList.toggle("active");
+});
+
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    nav.classList.remove("active");
+  });
+});
+
+//leffet de background qui change au defilement de la page
+window.addEventListener('scroll', function() {
+  var nav = document.querySelector('nav');
+  var logo = document.querySelector('nav img');
+  var scrollPosition = window.scrollY;
+  console.log(nav);
+  console.log(scrollPosition);
+
+
+  if (scrollPosition > 0) {
+    nav.style.backgroundColor = 'rgb(83, 5, 83)';
+    nav.style.transition = 'background-color 0.5s ease';
+    logo.style.width = '100px';
+    logo.style.height = '100px';
+    logo.style.transition = 'width 0.5s ease';
+  } else {
+    nav.style.backgroundColor = 'transparent';
+    nav.style.transition = 'background-color 0.5s ease';
+    logo.style.width = '300px';
+    logo.style.height = '250px';
+    logo.style.transition = 'width 0.5s ease';
   }
+});
+}

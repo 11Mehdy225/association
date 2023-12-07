309bc0e4e29989ae7b206132e0b970e54eab2107
unknown <loicmehdy11@outlook.fr>
2023-12-07 12:00:28 +0000

    version non responsive et formatage code_en cours

---
+++ js/js.js
@@ -0,0 +1,79 @@
+//variables globales
+let compteur = 0; //compteur qui permet de connaitre l'image affiché
+let timer , elements , slides, slidesWidth; 
+
+
+//charger le dom
+window.onload = () => {
+
+
+
+//recuperer le diapo
+var diapo = document.querySelector(".diapo");
+
+//recupere les elements
+elements = document.querySelector(".elements");
+console.log(elements);
+
+//on clone la premiere image pour qu'on puisse revenir a elle a la fin 
+let firstImage = elements.firstElementChild.cloneNode(true)
+
+//on injecte le clone a la fin du diapo 
+elements.appendChild(firstImage);
+
+slides = Array.from(elements.children);
+console.log(slides);
+
+//on recupere la largeur d'une slide
+slidesWidth = diapo.getBoundingClientRect().width;
+
+//on recupere les fleches pour leur attribuer leurs fonction
+let next = document.querySelector("#nav-droite");
+let prev = document.querySelector("#nav-gauche");
+//on gere le clic 
+next.addEventListener("click",slideNext);
+prev.addEventListener("click",slidePrev);
+
+//on automatise le defilement 
+timer=setInterval(slideNext,5000);
+
+//une fonction qui fait defiler les elements
+
+function slideNext(){ //fait defiler a droite
+    //incrementer le compteur
+    compteur++;
+    //transition des elements
+    elements.style.transition="1s linear";
+
+    let decal = -slidesWidth * compteur ;
+    elements.style.transform = `translateX(${decal}px)`;
+
+
+      //on attend la fin pour rembobiner
+ setTimeout(function() {
+     if (compteur >= slides.length - 1) {
+        compteur = 0 ;
+        //on enleve la transition et on revient à 0
+        elements.style.transition = "unset";
+        elements.style.transform = "translateX(0)";
+      }
+   },2000);
+}
+
+function slidePrev () { //fait defiler a gauche
+    //on decremente le compteur 
+    compteur--;
+    elements.style.transition="1s linear";
+
+    if (compteur < 0) {
+        compteur = slides.length -1 ;
+        let decal = -slidesWidth * compteur ;
+        elements.style.transition ="unset" ;
+        elements.style.transform = `translateX(${decal}px)`;
+        setTimeout(slidePrev,1);
+    }
+    
+    let decal = -slidesWidth * compteur ;
+    elements.style.transform = `translateX(${decal}px)`;
+    }
+  }
